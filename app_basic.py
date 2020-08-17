227963cec293450411999115eeb49cdab10fd5a0
Dilasha Shrestha <DShrestha@clarku.edu>
2020-08-17 12:30:20 -0400

    My first commit

--- app_basic.py
+++ app_basic.py
@@ -168,7 +168,7 @@ del df['Date']
 # Initialize the app
 app = dash.Dash(__name__)
 app.config.suppress_callback_exceptions = True
-server = app.server
+server = app.servergit push heroku master
 
 
 def get_options(list_stocks):
